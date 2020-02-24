0f2f545951d7ae65513ad6ab29af4a991a634cbc
Ana <eaaania@students.eaaa.dk>
2020-02-24 11:19:12 +0100

    Added the video and pics for  the gallery

--- js/js.js
+++ js/js.js
@@ -2,8 +2,5 @@ var theGallery = [ "DSC06397.JPG", "DSC06402.JPG", "DSC06406.JPG",
                  "DSC06395.JPG", "DSC06403.JPG", "DSC06398.JPG" ];
 
 for (i = 0; i < theGallery.length; i++) {
-	// note the shorthand code below
-  gallery.innerHTML += '<img src="images/'
-  + theGallery[i]
-  + '" alt="pictures from the store">';
+  gallery.innerHTML += '<img src="images/' + theGallery[i] + '"alt="pictures from the store" class="gallery_pics">';
 }
