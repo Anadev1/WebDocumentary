cfa621dc0698e09fcfbb46c3897cbe58556ec5c5
Ana <eaaania@students.eaaa.dk>
2020-02-26 11:17:32 +0100

    pls work btch

--- js/js.js
+++ js/js.js
@@ -88,19 +88,19 @@ var img_locker_home = document.getElementById('img_locker_home');
 var home = document.getElementById('mainvideo_container');
 
 elementBGhome.addEventListener('mouseover', function () {
-    img_locker_home.setAttribute("style", "content: url('../media/opened.png');")
+    img_locker_home.setAttribute("style", "content: url('opened.png');")
 });
 elementBGhome.addEventListener('mouseover', function () {
     elementBGhome.setAttribute("style", "background: white;");
 });
 elementBGhome.addEventListener('mouseout', function () {
-    img_locker_home.setAttribute("style", "content: url('../media/closed.png');")
+    img_locker_home.setAttribute("style", "content: url('closed.png');")
 });
 elementBGhome.addEventListener('mouseout', function () {
     elementBGhome.setAttribute("style", "background: lightgray;")
 });
 elementBGhome.addEventListener("click", function () {
-    img_locker_home.setAttribute("style", "content: url('../media/opened.png');")
+    img_locker_home.setAttribute("style", "content: url('opened.png');")
 });
 elementBGhome.addEventListener("click", function () {
     elementBGhome.setAttribute("style", "background: white;")
@@ -109,13 +109,13 @@ home.addEventListener("mouseover", function () {
     elementBGhome.setAttribute("style", "background: white;")
 });
 home.addEventListener("mouseover", function () {
-    img_locker_home.setAttribute("style", "content: url('../media/opened.png');")
+    img_locker_home.setAttribute("style", "content: url('opened.png');")
 });
 home.addEventListener('mouseout', function () {
     elementBGhome.setAttribute("style", "background: lightgray;")
 });
 home.addEventListener('mouseout', function () {
-    img_locker_home.setAttribute("style", "content: url('../media/closed.png');");
+    img_locker_home.setAttribute("style", "content: url('closed.png');");
 });
 
 
@@ -126,26 +126,26 @@ var img_locker_about = document.getElementById('img_locker_about');
 var about = document.getElementById('about');
 
 elementBGabout.addEventListener('mouseover', function () {
-    img_locker_about.setAttribute("style", "content: url('../media/opened.png');");
+    img_locker_about.setAttribute("style", "content: url('opened.png');");
     elementBGabout.setAttribute("style", "background: white;");
 });
 elementBGabout.addEventListener('mouseout', function () {
-    img_locker_about.setAttribute("style", "content: url('../media/closed.png');");
+    img_locker_about.setAttribute("style", "content: url('closed.png');");
     elementBGabout.setAttribute("style", "background: lightgray;");
 });
 elementBGabout.addEventListener("click", function () {
-    img_locker_about.setAttribute("style", "content: url('../media/opened.png');")
+    img_locker_about.setAttribute("style", "content: url('opened.png');")
 });
 elementBGabout.addEventListener("click", function () {
     elementBGabout.setAttribute("style", "background: white;")
 });
 about.addEventListener("mouseover", function () {
     elementBGabout.setAttribute("style", "background: white;");
-    img_locker_about.setAttribute("style", "content: url('../media/opened.png');")
+    img_locker_about.setAttribute("style", "content: url('opened.png');")
 });
 about.addEventListener('mouseout', function () {
     elementBGabout.setAttribute("style", "background: lightgray;");
-    img_locker_about.setAttribute("style", "content: url('../media/closed.png');");
+    img_locker_about.setAttribute("style", "content: url('closed.png');");
 });
 
 //work
@@ -155,24 +155,24 @@ var img_locker_work = document.getElementById('img_locker_work');
 var work = document.getElementById('videos_section');
 
 elementBGwork.addEventListener('mouseover', function () {
-    img_locker_work.setAttribute("style", "content: url('../media/opened.png');");
+    img_locker_work.setAttribute("style", "content: url('opened.png');");
     elementBGwork.setAttribute("style", "background: white;")
 });
 elementBGwork.addEventListener('mouseout', function () {
-    img_locker_work.setAttribute("style", "content: url('../media/closed.png');");
+    img_locker_work.setAttribute("style", "content: url('closed.png');");
     elementBGwork.setAttribute("style", "background: lightgray;")
 });
 elementBGwork.addEventListener("click", function () {
-    img_locker_work.setAttribute("style", "content: url('../media/opened.png');");
+    img_locker_work.setAttribute("style", "content: url('opened.png');");
     elementBGwork.setAttribute("style", "background: white;")
 });
 
 work.addEventListener("mouseover", function () {
-    img_locker_work.setAttribute("style", "content: url('../media/opened.png');");
+    img_locker_work.setAttribute("style", "content: url('opened.png');");
     elementBGwork.setAttribute("style", "background: white;")
 });
 work.addEventListener('mouseout', function () {
-    img_locker_work.setAttribute("style", "content: url('../media/closed.png');");
+    img_locker_work.setAttribute("style", "content: url('closed.png');");
     elementBGwork.setAttribute("style", "background: lightgray;")
 });
 
@@ -185,24 +185,24 @@ var img_locker_gallery = document.getElementById('img_locker_gallery');
 var gallery = document.getElementById('gallery_container');
 
 elementBGgallery.addEventListener('mouseover', function () {
-    img_locker_gallery.setAttribute("style", "content: url('../media/opened.png');");
+    img_locker_gallery.setAttribute("style", "content: url('opened.png');");
     elementBGgallery.setAttribute("style", "background: white;")
 });
 elementBGgallery.addEventListener('mouseout', function () {
-    img_locker_gallery.setAttribute("style", "content: url('../media/closed.png');");
+    img_locker_gallery.setAttribute("style", "content: url('closed.png');");
     elementBGgallery.setAttribute("style", "background: lightgray;")
 });
 elementBGgallery.addEventListener("click", function () {
-    img_locker_gallery.setAttribute("style", "content: url('../media/opened.png');");
+    img_locker_gallery.setAttribute("style", "content: url('opened.png');");
     elementBGgallery.setAttribute("style", "background: white;")
 });
 
 gallery.addEventListener("mouseover", function () {
-    img_locker_gallery.setAttribute("style", "content: url('../media/opened.png');");
+    img_locker_gallery.setAttribute("style", "content: url('opened.png');");
     elementBGgallery.setAttribute("style", "background: white;")
 });
 gallery.addEventListener('mouseout', function () {
-    img_locker_gallery.setAttribute("style", "content: url('../media/closed.png');");
+    img_locker_gallery.setAttribute("style", "content: url('closed.png');");
     elementBGgallery.setAttribute("style", "background: lightgray;")
 });
 
@@ -213,24 +213,24 @@ var img_locker_contact = document.getElementById('img_locker_contact');
 var contact = document.getElementById('contact');
 
 elementBGcontact.addEventListener('mouseover', function () {
-    img_locker_contact.setAttribute("style", "content: url('../media/opened.png');");
+    img_locker_contact.setAttribute("style", "content: url('opened.png');");
     elementBGcontact.setAttribute("style", "background: white;")
 });
 elementBGcontact.addEventListener('mouseout', function () {
-    img_locker_contact.setAttribute("style", "content: url('../media/closed.png');");
+    img_locker_contact.setAttribute("style", "content: url('closed.png');");
     elementBGcontact.setAttribute("style", "background: lightgray;")
 });
 elementBGcontact.addEventListener("click", function () {
-    img_locker_contact.setAttribute("style", "content: url('../media/opened.png');");
+    img_locker_contact.setAttribute("style", "content: url('opened.png');");
     elementBGcontact.setAttribute("style", "background: white;")
 });
 
 contact.addEventListener("mouseover", function () {
-    img_locker_contact.setAttribute("style", "content: url('../media/opened.png');");
+    img_locker_contact.setAttribute("style", "content: url('opened.png');");
     elementBGcontact.setAttribute("style", "background: white;")
 });
 contact.addEventListener('mouseout', function () {
-    img_locker_contact.setAttribute("style", "content: url('../media/closed.png');");
+    img_locker_contact.setAttribute("style", "content: url('closed.png');");
     elementBGcontact.setAttribute("style", "background: lightgray;")
 });
 
